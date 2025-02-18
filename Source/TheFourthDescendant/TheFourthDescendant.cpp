0ebcffb73da2cf3208d252dc3d3911c49a5e56a1
HyounJinDev <hyounjin.dev@gmail.com>
2025-02-18 20:31:25 +0900

    [Init] 프로젝트 생성 완료

---
+++ Source/TheFourthDescendant/TheFourthDescendant.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheFourthDescendant.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheFourthDescendant, "TheFourthDescendant" );
