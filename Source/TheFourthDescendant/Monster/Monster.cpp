176d945e978b4334b81b3acf59b391aa4f77e416
meo-young <meoyoung000@naver.com>
2025-02-19 19:17:37 +0900

    [Add] 적 액터가 상속받을 Monster 클래스 구현
    - 몬스터의 Status 및 Locomotion 변수 선언
    - Status, Locomotion에 해당하는 변수는 생성자에서 초기화 담당

---
+++ Source/TheFourthDescendant/Monster/Monster.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Monster.h"
+
+AMonster::AMonster()
+{
+	AttackPower = 0;
+	bCanAttack = false;
+	bIsSpawned = false;
+	bIsDead = false;
+}
+
+
