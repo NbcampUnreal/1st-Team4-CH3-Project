752c1e0e16a79a07c679a7a72f3e93b27a88e145
handakim25 <handakim25@gmail.com>
2025-02-19 19:58:17 +0900

    [Add] PlayerCharacter, ShooterPlayerController 생성
    - PlayerCharacter Camera, SpringArm 컴포넌트 추가
    - PlayerController로부터 PlayerCharacter의 함수에 바인딩

---
+++ Source/TheFourthDescendant/Player/PlayerCharacter.cpp
@@ -0,0 +1,208 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+
+#include "ShooterPlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	SpringArmComponent->TargetArmLength = 300.0f;
+	SpringArmComponent->bUsePawnControlRotation = true;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComponent->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
+	CameraComponent->bUsePawnControlRotation = false;
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (AShooterPlayerController* PlayerController = GetController<AShooterPlayerController>())
+		{
+			if (PlayerController->MoveAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->MoveAction,
+					ETriggerEvent::Triggered,
+					this,
+					&APlayerCharacter::Move
+				);
+			}
+
+			if (PlayerController->JumpAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->JumpAction,
+					ETriggerEvent::Started,
+					this,
+					&APlayerCharacter::StartJump
+				);
+			}
+
+			if (PlayerController->LookAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->LookAction,
+					ETriggerEvent::Triggered,
+					this,
+					&APlayerCharacter::Look
+				);
+			}
+
+			if (PlayerController->SprintAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->SprintAction,
+					ETriggerEvent::Started,
+					this,
+					&APlayerCharacter::Sprint
+				);
+			}
+
+			if (PlayerController->DodgeAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->DodgeAction,
+					ETriggerEvent::Started,
+					this,
+					&APlayerCharacter::Dodge
+				);
+			}
+
+			if (PlayerController->CrouchAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->CrouchAction,
+					ETriggerEvent::Started,
+					this,
+					&APlayerCharacter::StartCrouch
+				);
+			}
+
+			if (PlayerController->InteractionAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->InteractionAction,
+					ETriggerEvent::Triggered,
+					this,
+					&APlayerCharacter::Interaction
+				);
+			}
+
+			if (PlayerController->FireAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->FireAction,
+					ETriggerEvent::Started,
+					this,
+					&APlayerCharacter::StartShoot
+				);
+			}
+
+			if (PlayerController->FireAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->FireAction,
+					ETriggerEvent::Triggered,
+					this,
+					&APlayerCharacter::TriggerShoot
+				);
+			}
+
+			if (PlayerController->FireAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->FireAction,
+					ETriggerEvent::Completed,
+					this,
+					&APlayerCharacter::StopShoot
+				);
+			}
+
+			if (PlayerController->AimAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->AimAction,
+					ETriggerEvent::Started,
+					this,
+					&APlayerCharacter::StartAim
+				);
+			}
+
+			if (PlayerController->AimAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->AimAction,
+					ETriggerEvent::Completed,
+					this,
+					&APlayerCharacter::StopAim
+				);
+			}
+		}
+	}
+}
+
+void APlayerCharacter::Move(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::StartJump(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::Look(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::Sprint(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::Dodge(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::StartCrouch(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::Interaction(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::StartShoot(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::TriggerShoot(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::StopShoot(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::StartAim(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::StopAim(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::Reload(const FInputActionValue& Value)
+{
+}
---
+++ Source/TheFourthDescendant/Player/PlayerCharacter.h
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TheFourthDescendant/Abstracts/CharacterBase.h"
+#include "PlayerCharacter.generated.h"
+
+struct FInputActionValue;
+
+UCLASS()
+class THEFOURTHDESCENDANT_API APlayerCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+private:
+	/** TPS 카메라 컴포넌트 */
+	UPROPERTY(VisibleAnywhere)
+	class UCameraComponent* CameraComponent;
+	/** 카메라가 부착될 스프링 암 컴포넌트 */
+	UPROPERTY(VisibleAnywhere)
+	class USpringArmComponent* SpringArmComponent;
+
+public:
+	/** Mapping Context에 따라 입력을 바인딩*/
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	/** IA_Move 바인딩 함수 */
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
+	/** IA_Jump(Started) 바인딩 함수 */
+	UFUNCTION()
+	void StartJump(const FInputActionValue& Value);
+	/** IA_Look(Triggered) 바인딩 함수 */
+	UFUNCTION()
+	void Look(const FInputActionValue& Value);
+	/** IA_Sprint(Started) 바인딩 함수 */
+	UFUNCTION()
+	void Sprint(const FInputActionValue& Value);
+	/** IA_Dodge(Started) 바인딩 함수 */
+	UFUNCTION()
+	void Dodge(const FInputActionValue& Value);
+	/** IA_Crouch(Started) 바인딩 함수 */
+	UFUNCTION()
+	void StartCrouch(const FInputActionValue& Value);
+	/** IA_Interaction(Started) 바인딩 함수 */
+	UFUNCTION()
+	void Interaction(const FInputActionValue& Value);
+	/** IA_Fire(Started) 바인딩 함수 */
+	UFUNCTION()
+	void StartShoot(const FInputActionValue& Value);
+	/** IA_Fire(Triggered) 바인딩 함수 */
+	UFUNCTION()
+	void TriggerShoot(const FInputActionValue& Value);
+	/** IA_Fire(Completed) 바인딩 함수 */
+	UFUNCTION()
+	void StopShoot(const FInputActionValue& Value);
+	/** IA_Aim(Started) 바인딩 함수 */
+	UFUNCTION()
+	void StartAim(const FInputActionValue& Value);
+	/** IA_Aim(Completed) 바인딩 함수 */
+	UFUNCTION()
+	void StopAim(const FInputActionValue& Value);
+	/** IA_Reload(Started) 바인딩 함수 */
+	UFUNCTION()
+	void Reload(const FInputActionValue& Value);
+};
