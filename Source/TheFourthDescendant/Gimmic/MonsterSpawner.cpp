81d20b9f876fdfbe54ba45cc7c23b31970c5ad4c
ishyun1227 <ishyun1227@naver.com>
2025-02-21 16:44:44 +0900

    [Add] MonsterSpawner 추가
    - MonsterSpawer.cpp, MonsterSpawner.h 추가
    - MonsterSpawner 클래스 생성

--- Source/TheFourthDescendant/Gimmic/MonsterSpawner.cpp
+++ Source/TheFourthDescendant/Gimmic/MonsterSpawner.cpp
@@ -3,25 +3,148 @@
 
 #include "MonsterSpawner.h"
 
-// Sets default values
+#include "TheFourthDescendant/Monster/Melee/MeleeMonster.h"
+#include "TheFourthDescendant/Monster/Ranged/RangedMonster.h"
+
 AMonsterSpawner::AMonsterSpawner()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	FirstWaveSpawnData.Add(TTuple<EMonsterType, int32>(EMonsterType::Melee, 8));
+    SecondWaveSpawnData.Add(TTuple<EMonsterType, int32>(EMonsterType::Ranged, 4));
+    ThirdWaveSpawnData.Add(TTuple<EMonsterType, int32>(EMonsterType::Melee, 12));
+    ThirdWaveSpawnData.Add(TTuple<EMonsterType, int32>(EMonsterType::Ranged, 6));
 }
 
-// Called when the game starts or when spawned
+// (근접, 원거리) 스폰 위치에 몬스터 생성
+void AMonsterSpawner::Spawn(EMonsterType MonsterType)
+{
+	if (GetWorld())
+	{
+		FVector SpawnLocation = GetActorLocation(); 
+		FActorSpawnParameters SpawnParams;
+        
+		AMonster* NewMonster = nullptr;
+
+		switch (MonsterType)
+		{
+		case EMonsterType::Melee:
+			if (MeleeMonsterClass)
+			{
+				NewMonster = GetWorld()->SpawnActor<AMeleeMonster>(MeleeMonsterClass, SpawnLocation,FRotator::ZeroRotator, SpawnParams);
+			}
+			break;
+
+		case EMonsterType::Ranged:
+			if (RangedMonsterClass)
+			{
+				NewMonster = GetWorld()->SpawnActor<ARangedMonster>(RangedMonsterClass, SpawnLocation, FRotator::ZeroRotator, SpawnParams);
+			}
+			break;
+
+		case EMonsterType::Boss:
+			if (nullptr)
+			{
+				NewMonster = GetWorld()->SpawnActor<ARangedMonster>(nullptr, SpawnLocation, FRotator::ZeroRotator, SpawnParams);
+			}
+			break;
+			
+		}
+		
+		if (NewMonster)
+		{
+			// 추가 설정 로직이 필요한 경우 여기에 작성합니다.
+		}
+	}
+}
+
+// 몬스터 스폰 지점에 한 번에 생성
+void AMonsterSpawner::SpawnMonsters(TArray<TTuple<EMonsterType, int32>> SpawnData)
+{
+	for (const TTuple<EMonsterType, int32>& Data : SpawnData)
+	{
+		EMonsterType MonsterType = Data.Key;
+		int32 Count = Data.Value;
+
+		for (int32 i = 0; i < Count; ++i)
+		{
+			Spawn(MonsterType);
+		}
+	}
+}
+
+// 몬스터 랜덤 지점에 한 번에 생성
+void AMonsterSpawner::RandomSpawnMonsters(TArray<TTuple<EMonsterType, int32>> SpawnData)
+{
+	for (const TTuple<EMonsterType, int32>& Data : SpawnData)
+	{
+		EMonsterType MonsterType = Data.Key;
+		int32 Count = Data.Value;
+
+		for (int32 i = 0; i < Count; ++i)
+		{
+			RandomSpawn(MonsterType);
+		}
+	}
+}
+
+// (근접, 원거리) 몬스터 랜덤 위치 생성
+void AMonsterSpawner::RandomSpawn(EMonsterType MonsterType)
+{
+	if (GetWorld())
+	{
+		FVector SpawnLocation = GetRandomSpawnPoint(); 
+		FActorSpawnParameters SpawnParams;
+        
+		AMonster* NewMonster = nullptr;
+
+		switch (MonsterType)
+		{
+		case EMonsterType::Melee:
+			if (MeleeMonsterClass)
+			{
+				NewMonster = GetWorld()->SpawnActor<AMeleeMonster>(MeleeMonsterClass, SpawnLocation,FRotator::ZeroRotator, SpawnParams);
+			}
+			break;
+
+		case EMonsterType::Ranged:
+			if (RangedMonsterClass)
+			{
+				NewMonster = GetWorld()->SpawnActor<ARangedMonster>(RangedMonsterClass, SpawnLocation, FRotator::ZeroRotator, SpawnParams);
+			}
+			break;
+
+		case EMonsterType::Boss:
+			if (nullptr)
+			{
+				NewMonster = GetWorld()->SpawnActor<ARangedMonster>(nullptr, SpawnLocation, FRotator::ZeroRotator, SpawnParams);
+			}
+			break;
+			
+		}
+		
+		if (NewMonster)
+		{
+			// 추가 설정 로직이 필요한 경우 여기에 작성합니다.
+		}
+	}
+}
+
+FVector AMonsterSpawner::GetSpawnPoint()
+{
+	return GetActorLocation();
+}
+
+FVector AMonsterSpawner::GetRandomSpawnPoint()
+{
+	return FVector::ZeroVector;
+	// tarray fvector uproperty makeedit
+}
+
 void AMonsterSpawner::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
-void AMonsterSpawner::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
 
--- Source/TheFourthDescendant/Gimmic/MonsterSpawner.h
+++ Source/TheFourthDescendant/Gimmic/MonsterSpawner.h
@@ -4,7 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "TheFourthDescendant/Monster/Monster.h"
 #include "MonsterSpawner.generated.h"
+
+UENUM(BlueprintType)
+enum class EMonsterType : uint8
+{
+	Melee UMETA(DisplayName = "Melee"),
+	Ranged UMETA(DisplayName = "Ranged"),
+	Boss UMETA(DisplayName = "Boss")
+};
 
 UCLASS()
 class THEFOURTHDESCENDANT_API AMonsterSpawner : public AActor
@@ -12,15 +21,34 @@ class THEFOURTHDESCENDANT_API AMonsterSpawner : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	AMonsterSpawner();
 
+public:
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	/* 몬스터 스폰 주기 */
+	float SpawnInterval;
+	/* 몬스터 스폰 장소 */
+	TArray<FVector> SpawnPoints;
+	/* 스폰시킬 몬스터 */
+	TArray<AMonster> SpawnEnemy;
+	/* 스폰시킬 근거리 몬스터 클래스 */
+	TSubclassOf<AMonster> MeleeMonsterClass;
+	/* 스폰시킬 원거리 몬스터 클래스 */
+	TSubclassOf<AMonster> RangedMonsterClass;
+private:
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	void Spawn(EMonsterType MonsterType);
+	void SpawnMonsters(TArray<TTuple<EMonsterType, int32>> SpawnEnemy);
+	void RandomSpawn(EMonsterType MonsterType);
+	void RandomSpawnMonsters(TArray<TTuple<EMonsterType, int32>> SpawnEnemy);
+	FVector GetSpawnPoint();
+	FVector GetRandomSpawnPoint();
+	TArray<TTuple<EMonsterType, int32>> FirstWaveSpawnData;
+	TArray<TTuple<EMonsterType, int32>> SecondWaveSpawnData;
+	TArray<TTuple<EMonsterType, int32>> ThirdWaveSpawnData;
+protected:
+	virtual void BeginPlay() override;
+private:
 
 };
