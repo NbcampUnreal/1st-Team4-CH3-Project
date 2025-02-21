8c72e119c1981278fe3a5e9f6b8d590d24be470c
ishyun1227 <ishyun1227@naver.com>
2025-02-21 12:45:15 +0900

    [Add, Del] 몬스터스포너 클래스 생성, 게임모드 관련 클래스, 폴더 삭제
    - 몬스터스포너 클래스 생성
    - GameManager 폴더 삭제

--- Source/TheFourthDescendant/GameManager/GameStates/MainGameState.cpp
+++
@@ -1,2 +0,0 @@
-#include "D:\Git\1st-Team4-CH3-Project\Intermediate\Build\Win64\x64\TheFourthDescendantEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.Cpp20.h"
-#include "MainGameState.h"
--- Source/TheFourthDescendant/GameManager/GameStates/MainGameState.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class MainGameState
-{
-public:
-	
-};
