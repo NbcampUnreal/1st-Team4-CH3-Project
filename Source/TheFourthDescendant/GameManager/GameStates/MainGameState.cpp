1ec2c7c05766f9ef8d37b5b9e2ba58682c07d33f
ishyun1227 <ishyun1227@naver.com>
2025-02-21 11:30:05 +0900

    [Add] 게임 모드 관련 파일 생성
    - GameManager, GameStates, GameModes 폴더 생성
    - MainGameInstance, MainGameMode, MainGameState 클래스 생성

---
+++ Source/TheFourthDescendant/GameManager/GameStates/MainGameState.cpp
@@ -0,0 +1,2 @@
+#include "D:\Git\1st-Team4-CH3-Project\Intermediate\Build\Win64\x64\TheFourthDescendantEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.Cpp20.h"
+#include "MainGameState.h"
---
+++ Source/TheFourthDescendant/GameManager/MainGameInstance.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class MainGameInstance
+{
+public:
+	
+};
