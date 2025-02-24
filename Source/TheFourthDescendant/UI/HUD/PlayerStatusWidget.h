a7fd887667a8275625a6892c47210dfaa30eebcf
HyounJinDev <hyounjin.dev@gmail.com>
2025-02-24 14:50:34 +0900

    [Update] PlayerStatus Widget의 CPP 컨버팅 완료 및 테스트 완료

--- Source/TheFourthDescendant/UI/HUD/PlayerStatusWidget.h
+++ Source/TheFourthDescendant/UI/HUD/PlayerStatusWidget.h
@@ -2,18 +2,18 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/Widget.h"
+#include "Blueprint/UserWidget.h"
 #include "PlayerStatusWidget.generated.h"
 
 UCLASS()
-class THEFOURTHDESCENDANT_API UPlayerStatusWidget : public UWidget
+class THEFOURTHDESCENDANT_API UPlayerStatusWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
 public:
-	UPlayerStatusWidget();
+	void NativeOnInitialized();
 
-private:
+protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet")
 	int32 BulletInMag;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet")
@@ -25,37 +25,37 @@ private:
 	float SPPercent;
 
 public:
-	UFUNCTION(Category = "Setting Bullet Infos | InMagazine")
+	UFUNCTION(BlueprintCallable, Category = "Setting Bullet Infos | InMagazine")
 	void IncreaseBulletInMag();
-	UFUNCTION(Category = "Setting Bullet Infos | InMagazine")
+	UFUNCTION(BlueprintCallable, Category = "Setting Bullet Infos | InMagazine")
 	void DecreaseBulletInMag();
 	// 원하는 숫자만큼 현재 탄알집에 총알을 설정해준다.
-	UFUNCTION(Category = "Setting Bullet Infos | InMagazine")
-	void SettingBulletInMag(const int32 NewBulletCountInMag);
+	UFUNCTION(BlueprintCallable, Category = "Setting Bullet Infos | InMagazine")
+	void SettingBulletInMag(int32 NewBulletCountInMag);
 
 	// 원하는 숫자만큼 현재 탄알집에 있는 총알을 제외하고 총알을 위젯에 설정해준다.
-	UFUNCTION(Category = "Setting Bullet Infos | OtherTotal")
-	void SettingTotalMagsBullet(const int32 NewTotalMagsBulletCount);
+	UFUNCTION(BlueprintCallable, Category = "Setting Bullet Infos | OtherTotal")
+	void SettingTotalMagsBullet(int32 NewTotalMagsBulletCount);
 
 	// 편의성 함수, 전체 총알 수 중에서 원하는 만큼을 차감해서 탄알집에 넣어주는 효과
-	UFUNCTION(Category = "Setting Bullet Infos | Actions")
-	void Reload(const int32 DesireMagBulletCount);
+	UFUNCTION(BlueprintCallable, Category = "Setting Bullet Infos | Actions")
+	void Reload(int32 DesireMagBulletCount);
 
 	// Health Point ProgressBar의 퍼센트 설정, CurrentHP와 MaxHP를 입력해주면 내부에서 처리하여 변수에 대입해준다.
-	UFUNCTION(Category = "Setting ProgressBar")
-	void SetHPPercent(float CurrentHP, float MaxHP);
+	UFUNCTION(BlueprintCallable, Category = "Setting ProgressBar")
+	void SetHPPercentTwoParams(float CurrentHP, float MaxHP);
 	// Health Point ProgressBar의 퍼센트 설정, Percent를 단독으로 인자로 사용한다.
-	UFUNCTION(Category = "Setting ProgressBar")
-	void SetHPPercent(float Percent);
-	UFUNCTION(Category = "Setting ProgressBar")
-	float GetHPPercent();
+	UFUNCTION(BlueprintCallable, Category = "Setting ProgressBar")
+	void SetHPPercentOneParams(float Percent);
+	UFUNCTION(BlueprintCallable, Category = "Setting ProgressBar")
+	float GetHPPercent() const;
 
 	// Shield Point ProgressBar의 퍼센트 설정, CurrentSP와 MaxSP를 입력해주면 내부에서 처리하여 변수에 대입해준다.
-	UFUNCTION(Category = "Setting ProgressBar")
-	void SetSPPercent(float CurrentSP, float MaxSP);
+	UFUNCTION(BlueprintCallable, Category = "Setting ProgressBar")
+	void SetSPPercentTwoParams(float CurrentSP, float MaxSP);
 	// Shield Point ProgressBar의 퍼센트 설정, Percent를 단독으로 인자로 사용한다.
-	UFUNCTION(Category = "Setting ProgressBar")
-	void SetSPPercent(float Percent);
-	UFUNCTION(Category = "Setting ProgressBar")
-	float GetSPPercent();	
+	UFUNCTION(BlueprintCallable, Category = "Setting ProgressBar")
+	void SetSPPercentOneParams(float Percent);
+	UFUNCTION(BlueprintCallable, Category = "Setting ProgressBar")
+	float GetSPPercent() const;	
 };
