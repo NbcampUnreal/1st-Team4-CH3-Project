60137038887a62335bdfa1d83b637cfbd4c1d09b
HyounJinDev <hyounjin.dev@gmail.com>
2025-02-24 16:58:07 +0900

    [Add] HUD 클래스 생성

---
+++ Source/TheFourthDescendant/UI/HUD/MainHUD.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEFOURTHDESCENDANT_API UMainHUD : public UUserWidget
+{
+	GENERATED_BODY()
+};
