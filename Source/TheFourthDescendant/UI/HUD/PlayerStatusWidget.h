53e382074d0f0f12cd8d1dc9e01234dd8070893d
HyounJinDev <hyounjin.dev@gmail.com>
2025-02-24 09:24:48 +0900

    [Add] PlayerStatusWidget 생성
    - WBP_PlayerStatus를 CPP로 변환하기 위해서 소스 및 헤더 파일 생성

---
+++ Source/TheFourthDescendant/UI/HUD/PlayerStatusWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Widget.h"
+#include "PlayerStatusWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEFOURTHDESCENDANT_API UPlayerStatusWidget : public UWidget
+{
+	GENERATED_BODY()
+};
