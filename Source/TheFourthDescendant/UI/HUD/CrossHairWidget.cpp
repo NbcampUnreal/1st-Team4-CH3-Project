1cc153235f442b14c74fc782cedf03224eb1c70d
HyounJinDev <hyounjin.dev@gmail.com>
2025-02-24 15:37:03 +0900

    [Update] CrossHairWidget 구현 완료 및 컨버팅 완료

--- Source/TheFourthDescendant/UI/HUD/CrossHairWidget.cpp
+++ Source/TheFourthDescendant/UI/HUD/CrossHairWidget.cpp
@@ -3,7 +3,49 @@
 
 #include "CrossHairWidget.h"
 
+#include "Components/Image.h"
+
 void UCrossHairWidget::NativeOnInitialized()
 {
 	Super::NativeOnInitialized();
+
+	CrossHair_L = Cast<UImage>(GetWidgetFromName(TEXT("CrossHair_L")));
+	ensureMsgf(IsValid(CrossHair_L), TEXT("크로스헤어 왼쪽을 찾을 수 없음."));
+	CrossHair_L->SetRenderTransformAngle(0.f);
+
+	CrossHair_R = Cast<UImage>(GetWidgetFromName(TEXT("CrossHair_R")));
+	ensureMsgf(IsValid(CrossHair_R), TEXT("크로스헤어 오른쪽을 찾을 수 없음."));	
+	CrossHair_R->SetRenderTransformAngle(180.f);
+}
+
+void UCrossHairWidget::Shoot()
+{
+	IncreaseMovingTime();
+}
+
+void UCrossHairWidget::IncreaseMovingTime()
+{
+	RemainMovingTime += ShotPower;
+	if (RemainMovingTime > MaximumRemainTime)
+	{
+		RemainMovingTime = MaximumRemainTime;
+	}
+}
+
+void UCrossHairWidget::MoveCrossHair()
+{
+	if(RemainMovingTime <= 0)
+	{
+		return;
+	}
+
+	checkf(0 == MaximumRemainTime, TEXT("MaximumRemainTime은 0이 될 수 없습니다."));
+	
+	float MoveAlpha = RemainMovingTime / MaximumRemainTime;
+	CurrentMoveDistance = FMath::Lerp(0.f, MaximumMoveDistance, MoveAlpha);
+
+	CrossHair_L->SetRenderTranslation(FVector2D(-CurrentMoveDistance, 0.f));
+	CrossHair_R->SetRenderTranslation(FVector2D(CurrentMoveDistance, 0.f));
+	
+	RemainMovingTime -= MoveToOriginTime;
 }
--- Source/TheFourthDescendant/UI/HUD/CrossHairWidget.h
+++ Source/TheFourthDescendant/UI/HUD/CrossHairWidget.h
@@ -4,11 +4,41 @@
 #include "Blueprint/UserWidget.h"
 #include "CrossHairWidget.generated.h"
 
+class UImage;
+
 UCLASS()
 class THEFOURTHDESCENDANT_API UCrossHairWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
 public:
-	virtual void NativeOnInitialized() override; 
+	virtual void NativeOnInitialized() override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move Control")
+	float ShotPower;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move Control")
+	float MaximumRemainTime;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move Control")
+	float RemainMovingTime;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move Control")
+	float MoveToOriginTime;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move Control")
+	float MaximumMoveDistance;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move Control")
+	float CurrentMoveDistance;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component | Image")
+	TObjectPtr<UImage> CrossHair_L;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component | Image")
+	TObjectPtr<UImage> CrossHair_R;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Move Control")
+	void Shoot();
+
+private:
+	UFUNCTION()
+	void IncreaseMovingTime();
+	UFUNCTION()
+	void MoveCrossHair();	
 };
