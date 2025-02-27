74028619883e9fc957fd25aaa9cc3d3e63341599
meo-young <meoyoung000@naver.com>
2025-02-27 16:17:05 +0900

    [Add] 보스 좌우 움직임 Task 구현
    - Boss.h의 MoveHorizontal 함수와 연결

---
+++ Source/TheFourthDescendant/AI/Task/MoveHorizontal.cpp
@@ -0,0 +1,47 @@
+#include "MoveHorizontal.h"
+#include "AIController.h"
+#include "TheFourthDescendant/Monster/Boss/Boss.h"
+
+UMoveHorizontal::UMoveHorizontal()
+{
+	Boss = nullptr;
+	NodeName = "MoveHorizontal";
+	bNotifyTick = true;
+}
+
+
+
+EBTNodeResult::Type UMoveHorizontal::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	Boss = Cast<ABoss>(OwnerComp.GetAIOwner()->GetCharacter());
+	if (Boss == nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, "MoveHorizontal : Boss Casting Failed !");
+		return EBTNodeResult::Failed;
+	}
+
+	// 왼쪽, 오른쪽 중 움직일 방향 택
+	DirectionIndex = FMath::RandRange(0, 1);
+	
+	// 이동이 완료될 때까지 실행 유지
+	return EBTNodeResult::InProgress;
+}
+
+
+
+void UMoveHorizontal::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	// 보스가 BackMoving 상태가 끝났다면 성공 반환
+	if (Boss->MovementState != EBossMovementState::Surrounding)
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+		return;
+	}
+
+	if (Boss)
+	{
+		Boss->MoveHorizontal(DirectionIndex);
+	}
+}
