e315359497c382439c1a72480011843db1d0d26a
ishyun1227 <ishyun1227@naver.com>
2025-03-04 14:59:53 +0900

    [Update] Monster Spawner 모든 웨이브에서 일정 간격으로 스폰하도록 수정

--- Source/TheFourthDescendant/Gimmic/MonsterSpawner.cpp
+++ Source/TheFourthDescendant/Gimmic/MonsterSpawner.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MonsterSpawner.h"
 
 #include "Kismet/GameplayStatics.h"
@@ -19,10 +18,7 @@ AMonsterSpawner::AMonsterSpawner()
     CurrentMonsterCount = 0;
 }
 
-
-
-
-// (근접, 원거리) 스폰 위치에 몬스터 생성	
+// (근접, 원거리) 스폰 위치에 몬스터 생성  
 void AMonsterSpawner::Spawn(EMonsterType MonsterType, const FTransform& SpawnTransform)
 {
     if (GetWorld())
@@ -129,9 +125,6 @@ void AMonsterSpawner::RandomSpawn(EMonsterType MonsterType)
     }
 }
 
-
-
-
 // 여러 몬스터 스폰하는 함수
 void AMonsterSpawner::SpawnMonsters(int32 LevelIndex)
 {
@@ -167,62 +160,31 @@ void AMonsterSpawner::SpawnMonsters(int32 LevelIndex)
             CurrentMonsterType = Data.Key;
             int32 Count = Data.Value;  // 실제 생성해야 하는 몬스터 수
 
-            if (LevelIndex == 1)
+            // 모든 웨이브에서 일정 간격으로 스폰하도록 수정
+            GetWorld()->GetTimerManager().SetTimer(SpawnTimerHandle, [this, Count]()
             {
-                // 두 번째 웨이브: 타이머를 이용하여 일정 간격으로 랜덤 스폰
-                GetWorld()->GetTimerManager().SetTimer(SpawnTimerHandle, [this, Count]()
+                if (CurrentMonsterCount < Count)
                 {
-                    if (CurrentMonsterCount < Count)
-                    {
-                        RandomSpawn(CurrentMonsterType);
-                        CurrentMonsterCount++;
-                    }
-                    else
-                    {
-                        GetWorld()->GetTimerManager().ClearTimer(SpawnTimerHandle);
-                    }
-                }, SpawnInterval, true);
-            }
-            else
-            {
-                // 웨이브 1 및 웨이브 3: 지정된 스폰 포인트에서 동시에 스폰
-                for (int32 i = 0; i < Count; ++i)
-                {
-                    if (CurrentMonsterType == EMonsterType::Melee && MeleeTransformArrayPtr)
-                    {
-                        TArray<FTransform>& MeleeTransformArray = *MeleeTransformArrayPtr;
-                        if (i < MeleeTransformArray.Num())
-                        {
-                            const FTransform& SpawnTransform = MeleeTransformArray[i];
-                            Spawn(CurrentMonsterType, SpawnTransform);
-                        }
-                    }
-                    else if (CurrentMonsterType == EMonsterType::Ranged && RangedTransformArrayPtr)
-                    {
-                        TArray<FTransform>& RangedTransformArray = *RangedTransformArrayPtr;
-                        if (i < RangedTransformArray.Num())
-                        {
-                            const FTransform& SpawnTransform = RangedTransformArray[i];
-                            Spawn(CurrentMonsterType, SpawnTransform);
-                        }
-                    }
+                    RandomSpawn(CurrentMonsterType);
+                    CurrentMonsterCount++;
                 }
-            }
+                else
+                {
+                    GetWorld()->GetTimerManager().ClearTimer(SpawnTimerHandle);
+                }
+            }, SpawnInterval, true);
         }
     }
 }
 
 void AMonsterSpawner::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
     FirstWaveSpawnData.Add(TTuple<EMonsterType, int32>(EMonsterType::Melee, FirstWaveMonsterCount));
     SecondWaveSpawnData.Add(TTuple<EMonsterType, int32>(EMonsterType::Ranged, SecondWaveMonsterCount));
     ThirdWaveSpawnData.Add(TTuple<EMonsterType, int32>(EMonsterType::Melee, ThirdWaveMeleeCount));
     ThirdWaveSpawnData.Add(TTuple<EMonsterType, int32>(EMonsterType::Ranged, ThirdWaveRangedCount));
 
-	//SpawnMonsters(Levelindex);
+    //SpawnMonsters(Levelindex);
 }
-
-
-
