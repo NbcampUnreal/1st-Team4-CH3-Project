92a08d1fe78739f7f9ec65f64b5b3686c721fcdb
ishyun1227 <ishyun1227@naver.com>
2025-03-04 17:43:59 +0900

    [Add, Update] 몬스터가 아이템 드롭하도록 수정
    [Add] ItemSpawnRow 생성
    [Update] Monster가 죽을 때 아이템을 랜덤 스폰하도록 수정

--- Source/TheFourthDescendant/Monster/Monster.cpp
+++ Source/TheFourthDescendant/Monster/Monster.cpp
@@ -90,6 +90,50 @@ void AMonster::SpawnItem(TSubclassOf<AActor> ItemClass)
 		);
 }
 
+void AMonster::SpawnRandomItem()
+{
+	if (FItemSpawnRow* SelectedRow = GetRandomItem())
+	{
+		if (UClass* ActualClass = SelectedRow->ItemClass.Get())
+		{
+			SpawnItem(ActualClass);
+		}
+	}
+}
+
+FItemSpawnRow* AMonster::GetRandomItem() const
+{
+	if (!ItemDataTable) return nullptr;
+
+	TArray<FItemSpawnRow*> AllRows;
+	static const FString ContextString(TEXT("ItemSpawnContext"));
+	ItemDataTable->GetAllRows(ContextString, AllRows);
+
+	if (AllRows.IsEmpty()) return nullptr;
+
+	float TotalChance = 0.0f;
+	for (const FItemSpawnRow* Row : AllRows)
+	{
+		if (Row)
+		{
+			TotalChance += Row->SpawnChance;
+		}
+	}
+
+	const float RandValue = FMath::FRandRange(0.0f, TotalChance);
+	float AccumulateChance = 0.0f;
+
+	for (FItemSpawnRow* Row : AllRows)
+	{
+		AccumulateChance += Row->SpawnChance;
+		if (RandValue <= AccumulateChance)
+		{
+			return Row;
+		}
+	}
+	return nullptr;
+}
+
 void AMonster::OnDeath()
 {
 	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, "Monster OnDeath");
@@ -102,6 +146,8 @@ void AMonster::OnDeath()
 	bIsDead = true;
 	Blackboard->SetValueAsBool(FName("IsDead"), true);
 
+	SpawnRandomItem();
+	
 	// 죽인 적 수 반영
 	UGameInstance* GameInstance = GetGameInstance();
 	UMainGameInstance* MainGameInstance = Cast<UMainGameInstance>(GameInstance);
